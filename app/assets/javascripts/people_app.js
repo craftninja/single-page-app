bf3851a284f0a65a89a3417489d88182298772f5
Martha Berner <marthaberner.mb@gmail.com>
2014-06-11 11:46:42 -0600

    [#73065452] User can view all people on the index page
    Signed-off-by: Emily Platzer <emilyplatzer@gmail.com>

---
+++ app/assets/javascripts/people_app.js
@@ -0,0 +1,14 @@
+window.PeopleApp = {
+
+  initialize: function (path) {
+    $.getJSON(path, this.renderInitialPage);
+  },
+
+  renderInitialPage: function (response) {
+    $.each(response._embedded.people, function () {
+      var $html = $(JST['templates/person_show'](this));
+      $html.data('person', this);
+      $("[data-container=people]").append($html);
+    });
+  }
+}
